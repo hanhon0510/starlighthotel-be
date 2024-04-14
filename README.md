225587cc5406377733a7272e62b2731999c2068d
hanhon0510 <tranhieuams@gmail.com>
2024-04-15 00:14:27 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# starlighthotel
+ 
---
+++ src/main/java/com/hieu/starlighthotel/StarlighthotelApplication.java
@@ -0,0 +1,13 @@
+package com.hieu.starlighthotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarlighthotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarlighthotelApplication.class, args);
+	}
+
+}
